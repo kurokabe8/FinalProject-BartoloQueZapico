cdaa01079bc7b1b953d14e4ae378fad4cc4ac389
kurokabe8 <bartolomarco8@gmail.com>
2026-04-13 22:51:56 +0800

    models

--- Shemu/payroll_app/models.py
+++ Shemu/payroll_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    id_number = models.CharField(max_length=100, unique=True) 
+    rate = models.FloatField()
+    overtime_pay = models.FloatField(null=True, blank=True, default=0) 
+    allowance = model.FloatField(null=True, blank=True, default=0) 
+
+    def getName(self):
+        return self.name
+    
+    def getID(self):
+        return self.id_number
+    
+    def getRate(self):
+        return self.rate
+    
+    def getOvertime(self):
+        return self.overtime_pay
+    
+    def getAllowance(self):
+        return self.allowance
+    
+    def __str__(self):
+        return f"pk: {self.id_number}, rate: {self.rate}"
