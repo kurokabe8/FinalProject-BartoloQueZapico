4e7971141aaef1acbce357b06e7bfdd3cb85cd36
AGZ-boopy <andrew.g.zapico@gmail.com>
2026-04-21 03:05:22 +0800

    added separators in url and added payslip models + resetOvertime

--- Shemu/payroll_app/models.py
+++ Shemu/payroll_app/models.py
@@ -19,8 +19,74 @@ class Employee(models.Model):
     def getOvertime(self):
         return self.overtime_pay
     
+    def resetOvertime(self):
+        self.overtime_pay = 0
+        self.save()
+    
     def getAllowance(self):
         return self.allowance
     
     def __str__(self):
         return f"pk: {self.id_number}, rate: {self.rate}"
+
+class Payslip(models.Model):
+    id_number = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    month = models.CharField(max_length=100)
+    date_range = models.CharField(max_length=100)
+    year = models.CharField(max_length=4)
+    pay_cycle = models.IntegerField()
+    rate = models.FloatField()
+    earnings_allowance = models.FloatField(default=0)
+    overtime = models.FloatField(default=0)
+    deductions_tax = models.FloatField(default=0)
+    deductions_health = models.FloatField(default=0)
+    pag_ibig = models.FloatField(default=0)
+    sss = models.FloatField(default=0)
+    total_pay = models.FloatField(default=0)
+
+
+    def getIDNumber(self):
+        return self.id_number.id_number # first id number is foreignkey, 2nd one is from employee since namention na in relation sya
+
+    def getMonth(self):
+        return self.month
+
+    def getDate_range(self):
+        return self.date_range            
+
+    def getYear(self):
+        return self.year
+
+    def getPay_cycle(self):
+        return self.pay_cycle
+
+    def getRate(self):
+        return self.rate
+
+    def getCycleRate(self):
+        return self.rate / 2
+
+    def getEarnings_allowance(self):
+        return self.earnings_allowance
+
+    def getDeductions_tax(self):
+        return self.deductions_tax
+
+    def getDeductions_health(self):
+        return self.deductions_health
+
+    def getPag_ibig(self):
+        return self.pag_ibig
+
+    def getSSS(self):
+        return self.sss
+
+    def getOvertime(self):
+        return self.overtime
+
+    def getTotal_pay(self):
+        return self.total_pay
+    
+    def __str__(self):
+        return f"pk: {self.pk}, Employee: {self.id_number.id_number}, Period: {self.month} {self.date_range}, {self.year}, Cycle: {self.pay_cycle}, Total Pay: {self.total_pay}"
+    
--- Shemu/payroll_app/urls.py
+++ Shemu/payroll_app/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # employee 
     path('', views.employees_list, name='employees_list'),
     path('employees/create/', views.create_employee, name='create_employee'),
     path('employees/update/<int:pk>/', views.update_employee, name='update_employee'),
     path('employees/delete/<int:pk>/', views.delete_employee, name='delete_employee'),
     path('employees/add-overtime/<int:pk>/', views.add_overtime, name='add_overtime'),
+    
+    # payslip
     path('payslips/', views.payslips_list, name='payslips_list'),
     path('payslips/view/<int:pk>/', views.view_payslip, name='view_payslip'),
 ]
